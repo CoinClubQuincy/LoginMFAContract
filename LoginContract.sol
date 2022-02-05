0970c80bb8d8325c70da422016c7d53f96f30c8c
Q <rqjones@asu.edu>
2022-02-04 18:44:26 -0700

    Login

--- LoginContract.sol
+++ LoginContract.sol
@@ -6,14 +6,29 @@ import "./PrivateAccessToken.sol";
 contract LoginContract is PrivateAccessToken("Password","This is a passwordToken"){
     //new token that will allow for a smart contract to check credentials 
     uint PermissionToken =1;
+
+    mapping(uint => Logins) login_count;
     struct Logins{
         address DApp;
     }
 
     constructor(){}
     //I can grab the OnlyToken modifier from the Inherited contract to only allow to token holder to mint permission tokens
-    function MintPermission(address _DApp)public OnlyToken returns(bool){
-        _mint(_DApp, PermissionToken, 1, "");
-        return true;
+    function Login(string memory _password)public OnlyToken returns(bool){
+        
+        if(_password == PrivateData){
+            return true;
+        }
     }
+    function CheckUserCreds(address _user)public view returns(bool){
+        if(PrivateAccessToken.OnlyIf(_user) == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+contract SampleDApp{
+
 }
