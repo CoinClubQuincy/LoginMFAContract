c0d75a51cc96e3888ffe2ef45d55e41fff980cbc
Q <rqjones@asu.edu>
2022-02-04 19:05:35 -0700

    login

--- LoginContract.sol
+++ LoginContract.sol
@@ -3,24 +3,27 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./PrivateAccessToken.sol";
 
-contract LoginContract is PrivateAccessToken("Password","This is a passwordToken"){
+contract LoginContract is PrivateAccessToken{
     //new token that will allow for a smart contract to check credentials 
     uint PermissionToken =1;
+    string private Login_Pass;
 
     mapping(uint => Logins) login_count;
     struct Logins{
         address DApp;
     }
+    //lauch Login contract with PrivateAccessToken Contract as dependancy
+    constructor(string memory _Pass ,string memory _URI)PrivateAccessToken(Login_Pass,_URI){}
 
-    constructor(){}
     //I can grab the OnlyToken modifier from the Inherited contract to only allow to token holder to mint permission tokens
-    function Login()public view OnlyToken returns(bool){
-        if(1 == PermissionToken){
+    function Login(string memory _enterPass)public view OnlyToken returns(bool){
+        if(keccak256(abi.encodePacked(Login_Pass)) == keccak256(abi.encodePacked(_enterPass))){
             return true;
         } else{
             return false;
         }
     }
+    // external source can check if user data holds correct token
     function CheckUserCreds(address _user)public view returns(bool){
         if(PrivateAccessToken.OnlyIf(_user) == true){
             return true;
@@ -30,6 +33,6 @@ contract LoginContract is PrivateAccessToken("Password","This is a passwordToken
     }
 }
 
-contract SampleDApp{
+contract ExampleDApp{
 
 }
