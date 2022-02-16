e3019de05c33921e3e583a871ad9bfe2fe490e0a
Q <rqjones@asu.edu>
2022-02-15 17:57:47 -0700

    testing

--- test/loginDApp.js
+++ test/loginDApp.js
@@ -1,9 +1,22 @@
 const LoginContract = artifacts.require("LoginContract");
+const DAppLoginContract = artifacts.require("DAppLoginContract");
 
 contract('LoginContract',() => {
     it('Deploy Login Contract', async () => {
-        const loginContract = await LoginContract.deployed();
-        console.log(LoginContract.address);
+        const instance = await LoginContract.deployed()
+        const result = await(instance.Login("PASS")) 
+        //LoginContract.address
+        console.log(result);
         assert(LoginContract.address !== '');
+    });
+});
+
+contract('DAppLoginContract',() => {
+    it('Deploy Login Contract', async () => {
+        const instance = await DAppLoginContract.deployed()
+        //const result = await(instance.Login("PASS")) 
+        //LoginContract.address
+        console.log(instance.address);
+        assert(instance.address !== '');
     });
 });
