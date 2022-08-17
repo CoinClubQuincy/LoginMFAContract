eb86b4adf3f379e0b0c1e99d96614cf928bac2d7
Quincy Jones <rqjones@asu.edu>
2022-08-16 18:36:33 -0700

    Login on public mainnet

---
+++ contracts/PublicLoginContract.sol
@@ -0,0 +1,104 @@
+pragma solidity ^0.8.10;
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "./PublicPrivateAccessToken.sol";
+
+//interface for other contract to read status
+interface LoginContract_Interface{
+    function CheckUserCreds(address _user)external view returns(bool);
+    function login(string memory _y)external view returns(bool);
+}
+contract LoginContract is PublicPrivateAccessToken,LoginContract_Interface{
+    //new token that will allow for a smart contract to check credentials 
+    event ContractAddress(address _ContractSelf);
+
+
+    //lauch Login contract with PrivateAccessToken Contract as dependancy
+    constructor(string memory _Pass ,string memory _URI)PublicPrivateAccessToken(_Pass,_URI){
+        emit ContractAddress(address(this));
+    }
+
+
+    //I can grab the OnlyToken modifier from the Inherited contract
+    function User_Login(string memory _enterPass)public view OnlyToken returns(bool){
+        if(keccak256(abi.encodePacked(ViewData())) == keccak256(abi.encodePacked(keccak256(abi.encodePacked(_enterPass))))){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    // external source can check if user data holds correct token
+    function CheckUserCreds(address _user)public view returns(bool){
+        if(PublicPrivateAccessToken.OnlyIf(_user,1) == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //Dapp Contracts can check passwords
+    function login(string memory _enterPass)external view returns(bool){
+        if(keccak256(abi.encodePacked(_enterPass)) == keccak256(abi.encodePacked(PrivateViewData()))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+interface DAppLogin_Interface{
+    function userLogin(address _LoginContract,string memory _enterPass)external returns(bool);
+    function CredToken(address _LoginContract,address _user)external view returns(bool);
+    function Logout(address _LoginContract,address _user)external returns(string memory);
+    function LoginStatus(address _LoginContract) external view returns(bool);
+    function Register(address _LoginContract)external returns(bool);
+}
+//DApps can have this contract be the login contract for users to use this application
+contract DAppLoginContract is DAppLogin_Interface{
+    uint TotalAccounts =0;
+    constructor(){}
+    
+    mapping(address => Logins) logins;
+    //Keeps list of Login Contracts
+    struct Logins{
+        bool exist;
+        bool status;
+    }
+    //User can add their login contract to this DApp so this Daap can check their credentials in reference to the logincontract
+    function Register(address _LoginContract)public returns(bool){
+        if(logins[_LoginContract].exist == true){
+            return false;
+        }else{
+            logins[_LoginContract] =Logins(true,false);
+            TotalAccounts++;
+            return true;
+        }
+    }
+    //Checks Current Users Login Contracts
+    function LoginStatus(address _LoginContract) public view returns(bool){
+        return logins[_LoginContract].status;
+    }
+    //user Changes Login status
+    function Logout(address _LoginContract,address _user)public returns(string memory){
+        require(LoginContract_Interface(_LoginContract).CheckUserCreds(_user) == true);
+        logins[_LoginContract].status= false;
+        return "Logged out";
+    }
+    //user can login and have DApp check login COntract for validity
+    // contract address & password to login
+    function userLogin(address _LoginContract,string memory _enterPass)public returns(bool){
+        require(logins[_LoginContract].exist==true);
+        bool Password = LoginContract_Interface(_LoginContract).login(_enterPass);  //Has correct Password
+        bool Creds = LoginContract_Interface(_LoginContract).CheckUserCreds(msg.sender); //Has correct Credential Token
+        //Check Login Contract and Registration
+        if(Creds==true && Password==true){
+            logins[_LoginContract].status=true;
+            return true;  //user successfully logs in!
+        } else{
+            return false; //User fails to have correct NFT access or incorect password
+        }
+    }
+    //check user
+    function CredToken(address _LoginContract,address _user)public view returns(bool){
+        return LoginContract_Interface(_LoginContract).CheckUserCreds(_user);
+    }
+}
+//- Dev: Quincy J
---
+++ contracts/PublicPrivateAccessToken.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.8.10;
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract PublicPrivateAccessToken is ERC1155{
+    bytes32 private PrivateData;
+    uint Token =0;
+    uint totalNFTs=5;
+    // when contract is launched, PrivateData & URI will be defined
+    constructor(string memory _PrivateData,string memory URI)ERC1155(URI){
+        PrivateData = keccak256(abi.encodePacked(_PrivateData));
+        _mint(msg.sender, Token, totalNFTs, "");
+    }
+    //The modifier is so only the token holder is allowed to access function 
+    modifier OnlyToken(){
+        require(OnlyIf(msg.sender,1)==true);
+        _;
+    }
+    // 
+    modifier multi_Token(){
+        require(OnlyIf(msg.sender,3)==true);
+        _;
+    }
+    //The OnlyIf Function will run in the modifier to check if the user accessing the contract is the token holder
+    function OnlyIf(address _user,uint _tokenAmmount)internal view returns(bool){
+        if(balanceOf(_user,0)>=_tokenAmmount){ //changed from 1 to 3 for better security
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //This function will allow the Token Holder to view data
+    function ViewData() public multi_Token view returns(bytes32){
+        return PrivateData;
+    }
+    function PrivateViewData()internal view returns(bytes32){
+        return PrivateData;
+    }
+    //This function will check if current account has token
+    function ViewStatus()public view returns(uint){
+        return balanceOf(msg.sender,0);
+    }
+    //user can edit password if they hold 3 access tokens
+    function Edit(string memory _newData)public multi_Token returns(bool){
+         PrivateData = keccak256(abi.encodePacked(_newData));
+         return true;
+    }
+    //Burn compramsed tokens
+    function Burn(uint _amount, address _account)public multi_Token returns(string memory){
+            _burn(_account, Token, _amount);
+            return "burning {_amount} Credential tokens from {_account}";
+    }
+}
+
